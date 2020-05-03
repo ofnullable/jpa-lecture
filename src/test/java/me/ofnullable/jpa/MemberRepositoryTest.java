1ad6f070cf16f3c545409511e439b1b588bbe17b
ofnullable <ofnullable@gmail.com>
2020-05-04 03:13:26 +0900

    :tada: Initial commit

---
+++ src/test/java/me/ofnullable/jpa/MemberRepositoryTest.java
@@ -0,0 +1,41 @@
+package me.ofnullable.jpa;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    void save_member() throws Exception {
+        // given
+        var member = new Member();
+        member.setUsername("tester");
+
+        // when
+        Long saved = memberRepository.save(member);
+        var result = memberRepository.find(saved);
+
+        // then
+        assertThat(member.getId()).isEqualTo(result.getId());
+        assertThat(member.getUsername()).isEqualTo(result.getUsername());
+
+        assertThat(member).isEqualTo(result);
+    }
+
+}
