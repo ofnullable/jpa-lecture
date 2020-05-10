8a2e5731276b49d00d25587b749132c86230625a
ofnullable <ofnullable@gmail.com>
2020-05-10 20:03:00 +0900

    :sparkles: Add views and controllers

---
+++ src/main/java/me/ofnullable/jpa/common/HomeController.java
@@ -0,0 +1,17 @@
+package me.ofnullable.jpa.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        log.info("Welcome to Home!");
+        return "home";
+    }
+
+}
